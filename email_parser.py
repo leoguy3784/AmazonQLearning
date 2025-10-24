5c5e33165df988f0232b4578d4cea44ae5497368
leoguy3784 <leoguy3784@gmail.com>
2025-10-24 16:26:00 -0400

    copied the email_parser.py from the trainers git hub:

--- email_parser.py
+++ email_parser.py
@@ -1,5 +1,22 @@
 # Function to parse email:
 import email
+import extract_msg
 def parse_email(file_path):
     with open(file_path, 'r') as f:
-        print ()
+    # access email headers
+        print("From:", msg['From'])
+        print("To:", msg['To'])
+        print("Subject:", msg['Subject'].upper())
+        # access email body:
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == "text/plain":
+                    body = part.get_payload(decode=True).decode()
+                    print("Body:", body)
+
+        
+        else:
+            body = msg.get_payload(decode=True).decode()
+            print("Body:", body)
+
+parse_email('email.eml')
